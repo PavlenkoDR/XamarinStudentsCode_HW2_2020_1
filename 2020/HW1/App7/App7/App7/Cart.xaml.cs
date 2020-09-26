fd8ba95e43fe6eae12a9fc28620e724e8e9f4ae8
PavlenkoDR <pavlenkocraft@gmail.com>
2020-09-26 13:52:20 +1000

    HW1 init

---
+++ 2020/HW1/App7/App7/App7/Cart.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App7
+{
+    public class Good
+    {
+        public string name;
+        public int count;
+    }
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Cart : ContentPage
+    {
+        public static List<Good> goods = new List<Good>();
+        public Cart()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 2020/HW1/App7/App7/App7/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App7
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            treeeee.Clicked += (object sender, EventArgs e) => {
+            
+            
+            };
+
+
+            sub.ItemsSource = new[] {
+                "Text 1",
+                "Text 2",
+                "Text 3",
+                "Text 4"
+            };
+        }
+
+        private void sub_SelectedIndexChanged(object _sender, EventArgs _e)
+        {
+            description.Text = (_sender as Picker)?.SelectedItem.ToString();
+        }
+
+        private void stepper_ValueChanged(object _sender, ValueChangedEventArgs _e)
+        {
+            amount.Text = (_sender as Stepper)?.Value.ToString();
+        }
+
+        private void order_Clicked(object sender, EventArgs e)
+        {
+            Cart.goods.Add(new Good() { name = sub.SelectedItem.ToString(), count = int.Parse(amount.Text) });
+        }
+    }
+}
