482f703b2ed603d26f4b5f93bcb151451373e0d1
PavlenkoDR <pavlenkocraft@gmail.com>
2020-10-19 15:21:36 +1000

    HW2 init

---
+++ 2020/HW2/App7/App7/App7/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App7
+{
+    public class Notes {
+        public string Text { get; set; }
+    }
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Col1.ItemsSource = Instance.marks1;
+            Col2.ItemsSource = Instance.marks2;
+            Instance.marks1.CollectionChanged += (s, e) =>
+            {
+                Instance.l1 = Col1.Height;
+            };
+            Instance.marks2.CollectionChanged += (s, e) =>
+            {
+                Instance.l2 = Col2.Height;
+            };
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            button.IsEnabled = false;
+
+            var form = new Page1();
+            Navigation.PushAsync(form);
+            form.Disappearing += (send, ev) =>
+            {
+                button.IsEnabled = true;
+            };
+        }
+    }
+}
---
+++ 2020/HW2/App7/App7/App7/Page1.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App7
+{
+    static class Instance
+    {
+        static public ObservableCollection<Notes> marks1 = new ObservableCollection<Notes>();
+        static public ObservableCollection<Notes> marks2 = new ObservableCollection<Notes>();
+        static public double l1=0, l2=0;
+    }
+    
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var text = TextBox.Text;
+
+            if (text != null && text.Length != 0)
+            {
+                if(Instance.l1 <= Instance.l2)
+                    Instance.marks1.Add(new Notes() { Text = text });
+                else
+                    Instance.marks2.Add(new Notes() { Text = text });
+            }
+            Navigation.PopAsync();
+        }
+    }
+}
